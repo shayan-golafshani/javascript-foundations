7d9cba31a47100d6d05430684db608c29052e9ce
Shayan Golafshani <shayangolafsh@gmail.com>
2021-03-29 18:37:06 -0600

    Complete Dragon class and unskip test suite

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
+class Dragon {
+  constructor(name,rider){
+    //key value pairs
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.meals = 0;
+  }
+  //methods
+  greet(){
+    return `Hi, ${this.rider}!`
+  }
+  eat(){
+    this.meals++
+    if(this.meals >= 3){
+      this.hungry = false;
+    }
+  }
+}
 
+module.exports = Dragon

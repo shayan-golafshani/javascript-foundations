f077f2cae42e8fd1d399fb85bcaabcb028151755
Shayan Golafshani <shayangolafsh@gmail.com>
2021-04-05 15:38:00 -0600

    start to build a snowman

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,16 @@
 class Snowman {
-
+  constructor(snowman) {
+      this.carrots = snowman.carrots;
+      this.coal = snowman.coal;
+      this.buttons = snowman.buttons;
+      this.snowballs = snowman.snowballs;
+      this.magicHat = false;
+  }
+  canWearMagicHat() {
+    if (this.coal > 1) {
+      this.magicHat = true;
+    }
+  }
 }
 
 module.exports = Snowman;
